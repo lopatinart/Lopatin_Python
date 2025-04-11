8846075a2fe85aec5576cb21cedaf6f9296249e1
lopatinart <81517937+lopatinart@users.noreply.github.com>
2025-04-11 11:54:12 +0300

    Create PZ_12_1.py

---
+++ PZ_12/PZ_12_1.py
@@ -0,0 +1,25 @@
+# В последовательности на n целых чисел умножить все элементы на последний
+# минимальный элемент.
+
+import random
+
+n = int(input("Введите длину списка>> "))
+
+sequence = []
+
+for _ in range(n):
+    sequence.append(random.randint(1, 15))
+
+print(f"Начальный список>> {sequence}")
+
+
+min_element = min(sequence)
+
+last_min_index = len(sequence) - 1 - sequence[::-1].index(min_element)
+last_min_element = sequence[last_min_index]
+print(f"Индекс последнего минимального элемента>> {last_min_index}")
+print(f"Последний минимальный элемент>> {last_min_element}")
+
+
+result = [x * last_min_element for x in sequence]
+print(f"Конечный список>> {result}")
